9d8861220f14ae86e0864d98ba0bca4d838eb413
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2023-06-15 15:12:54 +0700

    menambahkan void pop

--- Stackarray/Stackarray.cpp
+++ Stackarray/Stackarray.cpp
@@ -25,7 +25,22 @@ public:
 		cout << element << "ditambahkan(pushed)" << endl;
 
 		return element;
-	};
+	}
+
+	void pop() {
+		if (empty()) { //step 1
+			cout << "\nStack is empty. cannot pop." << endl; //1.a
+			return; // 1.b
+		}
+
+		cout << "\nThe popped element is: " << stack_array[top] << endl; //step 2
+		top--; //step 3 decrement
+	}
+
+
+	
+
+	
 
 
 	
